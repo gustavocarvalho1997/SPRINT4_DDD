269cfe8226debc87dacce49f84b10a70a792bbfc
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-11-08 17:48:13 -0300

    Criada o modelo Guincho

---
+++ PortoApi/src/main/java/br/com/fiap/sprint4/models/Guincho.java
@@ -0,0 +1,53 @@
+package br.com.fiap.sprint4.models;
+
+import java.time.LocalDate;
+
+public final class Guincho extends Veiculo{
+	// Atributos
+	private int idGuincho;
+	private double maxToneladas;
+	private double maxComprimento;
+	private String descricao;
+	
+	// Construtores
+	public Guincho() {
+		super();
+	}
+	public Guincho(int id, String renavam, String modelo, String chassi, LocalDate dataVeiculo, double altura,
+			double comprimento, double largura, String cor, int idGuincho, double maxToneladas, double maxComprimento,
+			String descricao) {
+		super(id, renavam, modelo, chassi, dataVeiculo, altura, comprimento, largura, cor);
+		this.idGuincho = idGuincho;
+		this.maxToneladas = maxToneladas;
+		this.maxComprimento = maxComprimento;
+		this.descricao = descricao;
+	}
+	
+	// Getters
+	public int getIdGuincho() {
+		return idGuincho;
+	}
+	public double getMaxToneladas() {
+		return maxToneladas;
+	}
+	public double getMaxComprimento() {
+		return maxComprimento;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	// Setters
+	public void setIdGuincho(int idGuincho) {
+		this.idGuincho = idGuincho;
+	}
+	public void setMaxToneladas(double maxToneladas) {
+		this.maxToneladas = maxToneladas;
+	}
+	public void setMaxComprimento(double maxComprimento) {
+		this.maxComprimento = maxComprimento;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}//CLASS
