43d26a80ba7aea92f6650c93a592ad17d2a89eb5
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-11-09 23:17:46 -0300

    Ajuste classe Estado

--- PortoApi/src/main/java/br/com/fiap/sprint4/models/Estado.java
+++ PortoApi/src/main/java/br/com/fiap/sprint4/models/Estado.java
@@ -11,17 +11,14 @@ public final class Estado {
 	private String nome;
 	@JsonFormat(pattern = "dd-MM-yyyy", shape = JsonFormat.Shape.STRING)
 	private LocalDate dataCadastro;
-	//TODO verificar atributo
-	private String nomeUsuario;
 	
 	// Construtores
-	public Estado(int id, String sigla, String nome, LocalDate dataCadastro, String nomeUsuario) {
+	public Estado(int id, String sigla, String nome, LocalDate dataCadastro) {
 		super();
 		this.id = id;
 		this.sigla = sigla;
 		this.nome = nome;
 		this.dataCadastro = dataCadastro;
-		this.nomeUsuario = nomeUsuario;
 	}
 	public Estado() {
 		super();
@@ -40,10 +37,6 @@ public final class Estado {
 	public LocalDate getDataCadastro() {
 		return dataCadastro;
 	}
-	// TODO VERIFICAR
-	public String getNomeUsuario() {
-		return nomeUsuario;
-	}
 	
 	// Setters
 	public void setId(int id) {
@@ -57,10 +50,6 @@ public final class Estado {
 	}
 	public void setDataCadastro(LocalDate dataCadastro) {
 		this.dataCadastro = dataCadastro;
-	}
-	// TODO VERIFICAR
-	public void setNomeUsuario(String nomeUsuario) {
-		this.nomeUsuario = nomeUsuario;
 	}
 	
 	
