1de4c6c0a8f182516599eca83bb524832a56ffac
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-11-10 00:19:57 -0300

    Criada Exception pro caso do cadastro estar incompleto

---
+++ PortoApi/src/main/java/br/com/fiap/sprint4/exception/IncompleteRegistrationException.java
@@ -0,0 +1,8 @@
+package br.com.fiap.sprint4.exception;
+
+@SuppressWarnings("serial")
+public final class IncompleteRegistrationException extends Exception{
+	public IncompleteRegistrationException(String message) {
+		super(message);
+	}
+}//CLASS
