409652cc433d28f774007322cedad5bcd2836871
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-11-11 20:07:50 -0300

    CaminhaoResource: Adicionado atributo e construtor

--- PortoApi/src/main/java/br/com/fiap/sprint4/resource/CaminhaoResource.java
+++ PortoApi/src/main/java/br/com/fiap/sprint4/resource/CaminhaoResource.java
@@ -1,8 +1,16 @@
 package br.com.fiap.sprint4.resource;
 
+import java.sql.SQLException;
+
+import br.com.fiap.sprint4.service.CaminhaoService;
 import jakarta.ws.rs.Path;
 
 @Path("/caminhao")
 public final class CaminhaoResource {
-	
+	// Atributo
+	private CaminhaoService service;
+	// Construtor
+	public CaminhaoResource() throws ClassNotFoundException, SQLException {
+		service = new CaminhaoService();
+	}
 }//CLASS
