d275aa58ee5be6f565638c0785c2d38c73e0ce2f
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-11-11 16:19:42 -0300

    PlanoDeSeguroResource: Implementada a busca por ID

--- PortoApi/src/main/java/br/com/fiap/sprint4/resource/PlanoDeSeguroResource.java
+++ PortoApi/src/main/java/br/com/fiap/sprint4/resource/PlanoDeSeguroResource.java
@@ -2,8 +2,15 @@ package br.com.fiap.sprint4.resource;
 
 import java.sql.SQLException;
 
+import br.com.fiap.sprint4.exception.IdNotFoundException;
 import br.com.fiap.sprint4.service.PlanoDeSeguroService;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 
 @Path("/plano")
 public final class PlanoDeSeguroResource {
@@ -13,4 +20,16 @@ public final class PlanoDeSeguroResource {
 	public PlanoDeSeguroResource() throws ClassNotFoundException, SQLException {
 		service = new PlanoDeSeguroService();
 	}
+	
+	// PesquisarPorId INICIO
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response pesquisarPorId(@PathParam("id") int id) throws ClassNotFoundException, SQLException{
+		try {
+			return Response.ok(service.pesquisarPorId(id)).build();
+		} catch (IdNotFoundException e) {
+			return Response.status(Status.NOT_FOUND).entity(e.getMessage()).build();
+		}
+	}// PesquisarPorId FIM
 }//CLASS
