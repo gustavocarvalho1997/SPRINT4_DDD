44e4cb6c42a93594548a1cbaee260925f0294826
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-11-11 17:42:19 -0300

    EnderecoUtils criado e implementado parse

--- PortoApi/src/main/java/br/com/fiap/sprint4/exception/IncompleteRegistrationException.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.sprint4.exception;
-
-@SuppressWarnings("serial")
-public final class IncompleteRegistrationException extends Exception{
-	public IncompleteRegistrationException(String message) {
-		super(message);
-	}
-}//CLASS
